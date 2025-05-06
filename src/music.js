89a3c533749f74e1e34ede3f95c75d9e4980ad18
Jeici <varillitasporto123@gmail.com>
2025-05-06 00:00:30 -0500

    Mi Primer proyecto en GitHub

---
+++ src/music.js
@@ -0,0 +1,63 @@
+let allMusic = [
+    {
+        name: "Run - Red(Taylor's Version)",
+        artist: "Taylor Swift & Ed Shreeran",
+        img: "run",
+        src: "run"
+    },
+
+    {
+        name: "Perfect - Divide",
+        artist: "Ed Shreeran",
+        img: "perfect",
+        src: "perfect"
+    },
+
+    {
+        name: "Infinity - Feel Something",
+        artist: "Jaymes Young",
+        img: "infinity",
+        src: "infinity"
+    },
+
+    {
+        name: "No Lie - Now Dance",
+        artist: "Sean Paul & Dua Lipa",
+        img: "nolie",
+        src: "nolie"
+    },
+
+    {
+        name: "Sunflower - Spider-Man: Into the Spider-Verse",
+        artist: "Post Malone & Swae Lee",
+        img: "sunflower",
+        src: "sunflower"
+    },
+
+    {
+        name: "Baile inolvidable - Bad Bunny",
+        artist: "Bad Bunny",
+        img: "Baile inolvidable",
+        src: "baile inolvidable"
+    },
+
+    {
+        name: "DtMF - Bad Bunny",
+        artist: "Bad Bunny",
+        img: "DtMF",
+        src: "dtmf"
+    },
+
+    {
+        name: "Tu con el - Frankie Ruiz",
+        artist: "Frankie Ruiz",
+        img: "Tu con el",
+        src: "tu con el"
+    },
+    {
+        name: "Heat Waves - Dreamland",
+        artist: "Glass Animals",
+        img: "heatwaves",
+        src: "heatwaves"
+    }
+]
